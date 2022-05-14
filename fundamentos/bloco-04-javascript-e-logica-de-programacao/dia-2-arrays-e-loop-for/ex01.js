53534ed05bd2178432efb1685a6626a9df33de91
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2022-05-14 17:01:51 -0300

    adiciona pasta do bloco-04 e ex. realizados

---
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-2-arrays-e-loop-for/ex01.js
@@ -0,0 +1,6 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Obtenha o valor "Serviços" do array menu:
---
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-2-arrays-e-loop-for/ex13.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = 101;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] < menorNumero) {
+    menorNumero = numbers[i];
+  }
+}
+
+console.log(menorNumero);
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
