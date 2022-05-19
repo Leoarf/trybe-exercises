1754a48aa1c14be3e801170debe79f0b68eb077c
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2022-05-19 10:10:25 -0300

    arruma sintaxe

--- fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex14.js
+++ fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex14.js
@@ -4,6 +4,14 @@ let arrayInteiros = [2, 4, 6, 7, 10, 0, -3];
 
 menorValor (arrayInteiros);
 
-function menorValor () {
-    console.log(Math.min.apply(null, arrayInteiros));
+function menorValor (array) {
+    let menor = array[0];
+    let indice = 0;
+    for (let index = 1; index < array.length; index += 1) {
+        if (array[index] < menor) {
+            menor = array[index];
+            indice = index;
+        }
+    }
+    return indice;
 }
