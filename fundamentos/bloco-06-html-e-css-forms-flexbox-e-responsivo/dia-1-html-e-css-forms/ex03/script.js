04cc4004afef3d613a2419e746e88c7dc58f5ff7
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2022-05-31 15:55:49 -0300

    cumpre requisitos bonus do único exercício do dia

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/ex03/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/ex03/script.js
@@ -1,8 +1,27 @@
-const buttonsubmit = document.querySelector('#submit')
+const buttonsubmit = document.querySelector("#submit");
+const inputname = document.querySelector("#inputname");
+const inputemail = document.querySelector("#inputemail");
 
-buttonsubmit.addEventListener('click', function (event) {
-    event.preventDefault()
-})
+buttonsubmit.addEventListener("click", function (event) {
+  // event.preventDefault();
+  textInputValidation();
+});
+
+function textInputValidation() {
+  const email = document.querySelector("#inputemail").value.length;
+  const invalidEmail = email < 10;
+
+  const name = document.querySelector("#inputname").value.length;
+  const invalidName = name < 10;
+
+  if (invalidEmail || invalidName) {
+    window.alert("Dados Inválidos");
+  } else {
+    window.alert(
+      "Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip."
+    );
+  }
+}
 
 // O formulário deverá permitir que a pessoa usuária insira os seguintes campos:
 // Nome Completo:
@@ -22,3 +41,7 @@ buttonsubmit.addEventListener('click', function (event) {
 // Dica: Coloque a tag <script> no final do seu body.
 // Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). Nossa amiga Carol Silva nos contou um pouco sobre como fazer isso, lembra?
 // Crie um botão que limpe as informações contidas nos campos;
+
+// Bônus
+// A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+// Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
