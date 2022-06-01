29efc5e6c4a54263c82d6ef1e585e682c7a7765a
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2022-06-01 16:06:08 -0300

    adiciona pasta do dia e exercício obrigatório feito

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,34 @@
+const buttonsubmit = document.querySelector("#submit");
+const inputname = document.querySelector("#inputname");
+const inputemail = document.querySelector("#inputemail");
+const form = document.querySelector('#sortsection');
+const title = document.querySelector('#title')
+
+buttonsubmit.addEventListener("click", function (event) {
+  // event.preventDefault();
+  textInputValidation();
+});
+
+function textInputValidation() {
+  const email = document.querySelector("#inputemail").value.length;
+  const invalidEmail = email < 10;
+
+  const name = document.querySelector("#inputname").value.length;
+  const invalidName = name < 10;
+
+  if (invalidEmail || invalidName) {
+    window.alert("Dados Inválidos");
+  } else {
+    window.alert(
+      "Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip."
+    );
+  }
+}
+
+function changeFormStyle () {
+  form.style.padding = '50px'
+  title.style.textAlign = 'center'
+}
+
+changeFormStyle ()
+// Adicione um framework CSS de sua escolha ao formulário que você construiu na última aula e o utilize para estilizar o formulário.
